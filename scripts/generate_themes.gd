3d2d5dd0f39c78858474dd2b9c215ddc2dca79ca
Fahrstuhl <fahrstuhl@users.noreply.github.com>
2024-11-24 22:56:34 +0100

    add stub to generate themes with ThemeGen

---
+++ scripts/generate_themes.gd
@@ -0,0 +1,17 @@
+@tool
+extends ProgrammaticTheme
+
+func setup_desktop_theme():
+	set_save_path("res://themes/generated/desktop.theme")
+
+func setup_mobile_theme():
+	set_save_path("res://themes/generated/mobile.theme")
+
+func setup_solarized_light_theme():
+	set_save_path("res://themes/generated/solarized_light.theme")
+
+func setup_solarized_dark_theme():
+	set_save_path("res://themes/generated/solarized_dark.theme")
+
+func define_theme():
+	pass
