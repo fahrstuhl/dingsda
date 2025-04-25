6e13d6e94204cd9aace006acfd3b253ecd552aae
Fahrstuhl <fahrstuhl@users.noreply.github.com>
2025-04-25 16:25:00 +0200

    add static UI scale on Android

--- scripts/app.gd
+++ scripts/app.gd
@@ -4,6 +4,7 @@ func _ready():
 	print(theme)
 	if OS.get_name() == "Android":
 		OS.request_permissions()
+		get_window().content_scale_factor = 2
 	get_tree().get_root().size_changed.connect(resize)
 	resize()
 	create_and_open_recent_artefacts_document()
